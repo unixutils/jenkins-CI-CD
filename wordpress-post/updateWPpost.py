8ef1b3d5126f133a039e314aeb9881a4935639fb
vijayendar gururaja <admin@unixutils.com>
2022-02-27 10:25:30 +0000

    Add pipeline log URL to post

--- wordpress-post/updateWPpost.py
+++ wordpress-post/updateWPpost.py
@@ -25,9 +25,10 @@ now = datetime.now()
 current_time = now.strftime("%d/%m/%Y %H:%M:%S")
 
 content = """This post was updated using a CI/CD pipeline job run by jenkins on AWS @{0}.
-JOB_NAME = {1}
-BUILD_NUMBER = {2}
-BUILD_TAG = {3}""".format(current_time, JOB_NAME, BUILD_NUMBER, BUILD_TAG)
+JOB_NAME : {1}
+BUILD_NUMBER : {2}
+BUILD_TAG : {3}
+PIPELINE_LOG_URL : https://unixutils.com/ci-cd-live-pipeline-log/""".format(current_time, JOB_NAME, BUILD_NUMBER, BUILD_TAG)
 
 post = {
  'title'    : 'CI/CD Live',
